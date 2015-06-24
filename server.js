ec6bd8883918392b52558c3e4f62061d7b5c1f4b
Luiz Felipe vulgo Kaoi <kaoizin@gmail.com>
2015-06-24 08:42:36 -0300

    update codeship

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ var express = require( "express" );
 var app = require( "./config/express" )(app);
 var config = require('./config/config');
 
-http.createServer( app ).listen( config.port, config.address, function() {
+http.createServer( app ).listen( config.port, function() {
     console.log('Express Http Server ' +
         config.address + ' escutando na porta ' + config.port);
 });
 
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
-});
+//app.listen(port, function() {
+//    console.log('Our app is running on http://localhost:' + port);
+//});
